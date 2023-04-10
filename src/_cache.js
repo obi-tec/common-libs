896369b6f41003889c044245acb2cef80402a235
Bruno Camboim <bruno.camboim1997@gmail.com>
2023-04-10 14:22:10 -0300

    Added del and ttl to node-cache

--- src/_cache.js
+++ src/_cache.js
@@ -13,3 +13,11 @@ module.exports.set = (key, valeu, ttl) => {
 module.exports.get = (key) => {
   return global._cache.get(key);
 };
+
+module.exports.del = (key) => {
+  return global._cache.del(key);
+};
+
+module.exports.ttl = (key, ttl) => {
+  return global._cache.ttl(key, ttl);
+};
